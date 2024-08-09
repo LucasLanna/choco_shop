8814588296e3923825ae8b2912db4d4c3eb02715
Kuxixima Almeida <kuxialmeida@hotmail.com>
2024-08-09 02:32:16 +0100

    Update seed with image

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,6 +47,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :stock)
+    params.require(:product).permit(:name, :description, :price, :stock, photos: [])
   end
 end
